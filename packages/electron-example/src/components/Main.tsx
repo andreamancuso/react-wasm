5ce59cbca1964e7aa7d9cf9a0452c3416d5abd59
andreamancuso <and.mancuso@googlemail.com>
2024-05-15 18:10:56 +0200

    Update package.json, Main.tsx, and yarn.lock

--- packages/electron-example/src/components/Main.tsx
+++ packages/electron-example/src/components/Main.tsx
@@ -1,34 +1,21 @@
-import { ReactImgui } from "@react-wasm/dear-imgui";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore abc
+import getWasmModule from "@react-wasm/dear-imgui/dist/reactDearImgui.mjs";
+import { ReactImgui, ImGuiDemo } from "@react-wasm/dear-imgui";
 import { useRef } from "react";
 
 import "./Main.css";
 
 const Main = () => {
     const containerRef = useRef<HTMLDivElement>(null);
-    
+
     return (
         <div id="app" ref={containerRef}>
-      <ReactImgui containerRef={containerRef}>
-        <ReactImgui.CollapsingHeader label="Help">
-          <ReactImgui.SeparatorText label="ABOUT THIS DEMO:" />
-          <ReactImgui.BulletText text="Sections below are demonstrating many aspects of the library." />
-          <ReactImgui.BulletText text='The "Examples" menu above leads to more demo contents.' />
-          <ReactImgui.BulletText
-              text={`The "Tools" menu above gives access to: About Box, Style Editor,\nand Metrics/Debugger (general purpose Dear ImGui debugging tool).`}
-          />
-          <ReactImgui.SeparatorText label="PROGRAMMER GUIDE:" />
-          <ReactImgui.BulletText text="See the ShowDemoWindow() code in imgui_demo.cpp. <- you are here!" />
-          <ReactImgui.BulletText text="See comments in imgui.cpp." />
-          <ReactImgui.BulletText text="See example applications in the examples/ folder." />
-          <ReactImgui.BulletText text="Read the FAQ at https://www.dearimgui.com/faq/" />
-          <ReactImgui.BulletText text="Set 'io.ConfigFlags |= NavEnableKeyboard' for keyboard controls." />
-          <ReactImgui.BulletText text="Set 'io.ConfigFlags |= NavEnableGamepad' for gamepad controls." />
-
-          <ReactImgui.SeparatorText label="USER GUIDE:" />
-        </ReactImgui.CollapsingHeader>
-      </ReactImgui>
-    </div>
+            <ReactImgui getWasmModule={getWasmModule} containerRef={containerRef}>
+                <ImGuiDemo />
+            </ReactImgui>
+        </div>
     );
-}
+};
 
 export default Main;
