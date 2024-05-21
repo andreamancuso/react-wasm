40e647b36c48551e19670929f87fb7db11c499cb
Andrea Mancuso <and.mancuso@googlemail.com>
2024-05-21 14:24:46 +0200

    Update App.tsx and nativeFabricUiManager.js

--- packages/egui/ts/src/App.tsx
+++ packages/egui/ts/src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useMemo, useRef } from "react";
 import { ReactEgui } from "./lib/components/ReactEgui/components";
 import initWasmModule, { set_widget, init_egui } from "./lib/wasm/eframe_template";
 
@@ -13,11 +13,16 @@ const module: any = {
 function App() {
     const containerRef = useRef<HTMLDivElement>(null);
 
+    // const arr = useMemo(() => Array.from({ length: 1000 }), []);
+
     return (
         <div id="app" ref={containerRef}>
             <ReactEgui initWasmModule={module} containerRef={containerRef}>
                 <ReactEgui.Button label="Hello, world!" />
                 <ReactEgui.InputText defaultValue="Hello, world!" />
+                {/* {arr.map((_, index) => (
+                    <ReactEgui.InputText key={index} defaultValue="Hello, world!" />
+                ))} */}
             </ReactEgui>
         </div>
     );
--- packages/egui/ts/src/lib/react-native/nativeFabricUiManager.js
+++ packages/egui/ts/src/lib/react-native/nativeFabricUiManager.js
@@ -25,7 +25,7 @@ export default class {
         const { children, type, id, ...props } = payload;
         const widget = { ...props, id: generatedId, type };
 
-        console.log(JSON.stringify(widget));
+        // console.log(JSON.stringify(widget));
 
         this.wasmModule.set_widget(JSON.stringify(widget));
 
