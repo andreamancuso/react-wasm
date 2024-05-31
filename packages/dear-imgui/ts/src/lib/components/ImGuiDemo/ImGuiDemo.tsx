acefcb6bb6a28426ecb358bfd341cc8054017cec
Andrea Mancuso <and.mancuso@googlemail.com>
2024-05-31 18:41:17 +0000

    Amended behaviour of add text to multi line text renderer so that 100 lines of text are sent at once

--- packages/dear-imgui/ts/src/lib/components/ImGuiDemo/ImGuiDemo.tsx
+++ packages/dear-imgui/ts/src/lib/components/ImGuiDemo/ImGuiDemo.tsx
@@ -66,7 +66,9 @@ export const ImGuiDemo = () => {
     const handleAppendTextToTextRenderer = useCallback(() => {
         if (clippedMultiLineTextRendererRef.current) {
             clippedMultiLineTextRendererRef.current.appendTextToClippedMultiLineTextRenderer(
-                `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n`,
+                `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n`.repeat(
+                    100,
+                ),
             );
         }
     }, [clippedMultiLineTextRendererRef]);
