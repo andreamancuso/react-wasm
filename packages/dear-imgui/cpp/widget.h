2f31944dc82e9983185f2dc1677e606fcf7b22a5
andreamancuso <and.mancuso@googlemail.com>
2024-06-05 15:59:44 +0200

    Somehow undid some changes when as my git folder got corrupted

--- packages/dear-imgui/cpp/widget.h
+++ packages/dear-imgui/cpp/widget.h
@@ -51,7 +51,11 @@ class Widget {
 
         void HandleChildren(ReactImgui* view);
 
+        virtual void PreRender(ReactImgui* view);
+
         virtual void Render(ReactImgui* view) = 0;
+
+        virtual void PostRender(ReactImgui* view);
 
         virtual void Patch(const json& val) = 0;
 
@@ -69,6 +73,14 @@ class StyledWidget : public Widget {
         StyledWidget(int id) : Widget(id) {
             m_style = std::make_unique<Style>();
         }
+
+        void PreRender(ReactImgui* view);
+
+        void PostRender(ReactImgui* view);
+
+        bool HasCustomFont(ReactImgui* view);
+
+        bool HasCustomColor();
 };
 
 class Fragment final : public Widget {
@@ -351,9 +363,9 @@ class UnformattedText final : public StyledWidget {
             m_style->maybeColor = std::get<1>(style);
         }
 
-        ImGuiCol GetImGuiCol();
-
         void Render(ReactImgui* view);
+
+        ImGuiCol GetImGuiCol();
 
         void Patch(const json& val) {
             if (val.is_object()) {
