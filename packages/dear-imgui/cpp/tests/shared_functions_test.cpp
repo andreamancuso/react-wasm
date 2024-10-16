1c511adfa3d874fac58ec54cade3fff2a83856a6
andreamancuso <and.mancuso@googlemail.com>
2024-10-16 16:58:38 +0200

    Update shared_functions_test.cpp

--- packages/dear-imgui/cpp/tests/shared_functions_test.cpp
+++ packages/dear-imgui/cpp/tests/shared_functions_test.cpp
@@ -1,8 +1,21 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 #include "shared.h"
 
-// Demonstrate some basic assertions.
-TEST(SharedFunctionsTest, charPercentageToFloat) {
-    EXPECT_EQ(charPercentageToFloat("100%"), 100.0f);
+using ::testing::IsTrue;
+using ::testing::Eq;
+using ::testing::FloatEq;
+
+TEST(charPercentageToFloat, handlesPercentages) {
+    auto maybeResult = charPercentageToFloat("100%");
+
+    EXPECT_THAT(maybeResult.has_value(), IsTrue());
+    EXPECT_THAT(maybeResult.value(), FloatEq(100.0f));
+}
+
+TEST(charPercentageToFloat, handlesInvalidValues) {
+    auto result = charPercentageToFloat("%");
+
+    EXPECT_THAT(result, Eq(std::nullopt));
 }
